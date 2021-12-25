a1eb53a53a245e9d7e338b294ea3e95b2c2d1fe9
kush-s <26009914+Kush-S@users.noreply.github.com>
2021-12-24 19:52:55 -0500

    Change parseZybooks.py file to use primary email instead of school to avoid null values

--- public/python_scripts/parseZybooks.py
+++ public/python_scripts/parseZybooks.py
@@ -3,5 +3,5 @@ import sys
 
 data_frame = pandas.read_csv(str(sys.argv[1]))
 
-json = data_frame[['Last name', 'First name', 'School email']].to_json(orient='index')
+json = data_frame[['Last name', 'First name', 'Primary email']].to_json(orient='index')
 print(json)
