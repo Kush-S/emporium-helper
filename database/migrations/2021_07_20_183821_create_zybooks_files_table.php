1b931dc4dc4ed348c23f38e1d613c3fd863e7e0c
kush-s <26009914+Kush-S@users.noreply.github.com>
2022-01-07 19:09:53 -0500

    Add parsed column to File table and cast it as array

--- database/migrations/2021_07_20_183821_create_zybooks_files_table.php
+++ database/migrations/2021_07_20_183821_create_zybooks_files_table.php
@@ -16,6 +16,7 @@ class CreateZybooksFilesTable extends Migration
         Schema::create('zybooks_files', function (Blueprint $table) {
             $table->id();
             $table->string('name');
+            $table->json('parsed');
             $table->timestamps();
         });
     }
