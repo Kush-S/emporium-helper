e1378946b02a448a43ba8acd7080578dc5768e2f
kush-s <26009914+Kush-S@users.noreply.github.com>
2022-01-10 14:40:38 -0500

    Remove job commands, return parse info to file controller

--- app/Http/Controllers/ZybooksFileController.php
+++ app/Http/Controllers/ZybooksFileController.php
@@ -37,7 +37,7 @@ class ZybooksFileController extends Controller
             ->storeAs(Config::get('emporium_variables.storage_directory'), $request->file_name . '.csv');
 
     // call parseFile() on uploaded file
-    $this->parseFile($request);
+    $this->parseFile();
     return redirect()->route('files_index');
   }
 
@@ -58,19 +58,35 @@ class ZybooksFileController extends Controller
     return redirect()->route('files_index');
   }
 
-  private function parseFile(Request $request)
+  private function parseFile()
   {
-    // clean file_name in case of spaces, etc.
-    $file_name = escapeshellarg($request->file_name);
+    foreach(ZybooksFile::all() as $file){
+      if ($file->parsed["student_info"] == false){
+        $file_name = escapeshellarg($file->name);
 
-    // student info - build and run python command
-    $shell_command = "python python_scripts/parseStudents.py ../storage/app/zybooks_files/" . $file_name . ".csv";
-    $output = shell_exec($shell_command);
+        // student info - build and run python command
+        $shell_command = "python python_scripts/parseStudents.py ../storage/app/zybooks_files/" . $file_name;
+        error_log($shell_command);
+        $output_json = shell_exec($shell_command);
 
-    // decode output from python to JSON
-    $output_json = json_decode($output, true);
-    // $this->parseStudentInfo($output_json);
+        // decode output from python to JSON
+        $output_json = json_decode($output_json, true);
+        
+        // store each student, if already not stored
+        foreach ($output_json as $info)
+        {
+          $student = Student::firstOrCreate([
+            'first_name' => $info['First name'],
+            'last_name' => $info['Last name'],
+            'email' => $info['Primary email']
+          ]);
+        }
 
-    ProcessStudentGeneralInfo::dispatch($output_json);
+        $temp = $file->parsed;
+        $temp["student_info"] = true;
+        $file->parsed = $temp;
+        $file->save();
+      }
+    }
   }
 }
--- app/Jobs/ProcessStudentGeneralInfo.php
+++ app/Jobs/ProcessStudentGeneralInfo.php
@@ -9,20 +9,23 @@ use Illuminate\Foundation\Bus\Dispatchable;
 use Illuminate\Queue\InteractsWithQueue;
 use Illuminate\Queue\SerializesModels;
 use App\Models\Student;
+use App\Models\ZybooksFile;
+use Illuminate\Support\Facades\Storage;
+use Config;
 
 class ProcessStudentGeneralInfo implements ShouldQueue
 {
     use Dispatchable, InteractsWithQueue, Queueable, SerializesModels;
 
-    protected $output_json;
+    // protected $output_json;
     /**
      * Create a new job instance.
      *
      * @return void
      */
-    public function __construct($json)
+    public function __construct()
     {
-      $this->output_json = $json;
+      // $this->output_json = $json;
     }
 
     /**
@@ -32,14 +35,29 @@ class ProcessStudentGeneralInfo implements ShouldQueue
      */
     public function handle()
     {
-      # store each student, if already not stored
-      foreach ($this->output_json as $info)
-      {
-        $student = Student::firstOrCreate([
-          'first_name' => $info['First name'],
-          'last_name' => $info['Last name'],
-          'email' => $info['Primary email']
-        ]);
+      foreach(ZybooksFile::all() as $file){
+        if ($file->parsed["student_info"] == false){
+          $file_name = escapeshellarg($file->name);
+
+          // student info - build and run python command
+          $shell_command = "python " . Config::get('emporium_variables.storage_directory') . "/storage/python_scripts/parseStudents.py ../storage/app/zybooks_files/" . $file_name;
+          error_log($shell_command);
+          $output_json = shell_exec($shell_command);
+
+          // decode output from python to JSON
+          $output_json = json_decode($output_json, true);
+          // error_log($output_json);
+          // store each student, if already not stored
+
+          foreach ($output_json as $info)
+          {
+            $student = Student::firstOrCreate([
+              'first_name' => $info['First name'],
+              'last_name' => $info['Last name'],
+              'email' => $info['Primary email']
+            ]);
+          }
+        }
       }
     }
 }
