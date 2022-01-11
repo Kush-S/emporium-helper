8291c020d80cedc7613f7aae1d8f167ff043ee6f
kush-s <26009914+Kush-S@users.noreply.github.com>
2022-01-11 15:53:49 -0500

    Add recalculate risk button to statistics page

--- resources/views/statistics.blade.php
+++ resources/views/statistics.blade.php
@@ -1,12 +1,17 @@
 <x-headers/>
 
 <div class="container bg-light">
-  <div class="p-5 d-flex justify-content-center">
-    <h4>Stats page</h4>
+  <div class="row">
+    <div class="pt-2">
+      <button class="btn btn-danger float-end">Recalculate risk</button>
+    </div>
   </div>
 
 
+  <div class="row">
+    <div class="p-5 d-flex justify-content-center">
+      <h4>Stats page</h4>
+    </div>
 
-
-
+  </div>
 </div>
