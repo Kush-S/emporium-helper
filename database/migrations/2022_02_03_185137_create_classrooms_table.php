eba591cf20c27aeb1501e2e9693e37ce0fabba75
kush-s <26009914+Kush-S@users.noreply.github.com>
2022-02-03 20:47:04 -0500

    Add year to classroom migration

--- database/migrations/2022_02_03_185137_create_classrooms_table.php
+++ database/migrations/2022_02_03_185137_create_classrooms_table.php
@@ -18,6 +18,7 @@ class CreateClassroomsTable extends Migration
             $table->string('term');
             $table->string('number');
             $table->string('section');
+            $table->integer('year');
             $table->timestamps();
         });
     }
