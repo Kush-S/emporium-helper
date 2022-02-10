09be46a58d53bb6393ce88be650a5c4436ef6712
kush-s <26009914+Kush-S@users.noreply.github.com>
2022-02-09 22:42:06 -0500

    Style classroom search page and add link to go back

--- resources/views/classroom_search.blade.php
+++ resources/views/classroom_search.blade.php
@@ -7,12 +7,13 @@
             <div class="card">
                 <div class="card-header">
                   <div class="float-start">Classes</div>
-                  <a href="#" class="float-end">Search <i class="bi bi-search fas fa-2x"></i></a>
+                  <a href="{{ route("classroom_search_index") }}" class="float-end">Search <i class="bi bi-search fas fa-2x"></i></a>
                 </div>
-                <div class="container">
-                  <div class="row">
-                    <form>
-                      <div class="mb-3">
+
+                <form>
+                  <div class="container">
+                    <div class="row">
+                      <div class="col mb-3">
                         <label class="form-label">Term</label>
                         <select name="term" class="form-select">
                           <option {{$searchdata['term'] == '' ? "selected" : ""}}></option>
@@ -21,41 +22,46 @@
                           <option {{$searchdata['term'] == 'Fall' ? "selected" : ""}}>Fall</option>
                         </select>
                       </div>
-                      <div class="mb-3">
+                      <div class="col mb-3">
                         <label class="form-label">Year</label>
                         <input class="form-control" name="year" value="{{$searchdata['year'] ?? ""}}">
                       </div>
-                      <div class="mb-3">
+                      <div class="col mb-3">
                         <label class="form-label">Course Number</label>
                         <input class="form-control" name="number" value="{{$searchdata['number'] ?? ""}}">
                       </div>
-                      <div class="mb-3">
+                      <div class="col mb-3">
                         <label class="form-label">Section</label>
                         <input class="form-control" name="section" value="{{$searchdata['section'] ?? ""}}">
                       </div>
+                    </div>
+                    <div class="float-end">
+                      <a href="{{route("classroom_index")}}" class="btn btn-danger">Back to Class List</a>
                       <button type="submit" class="btn btn-primary">Search</button>
-                    </form>
+                    </div>
+
                   </div>
-                </div>
+                </form>
+
                 <div class="card-body">
                   <table class="table table-striped table-hover">
                     <thead>
                       <tr>
                         <th scope="col">Term</th>
                         <th scope="col">Year</th>
-                        <th scope="col">Number</th>
+                        <th scope="col">Course Number</th>
                         <th scope="col">Section</th>
                       </tr>
                     </thead>
                     <tbody>
                       @foreach ($classrooms as $classroom)
-                          <tr>
-                            <td>{{ $classroom->term }}</td>
-                            <td>{{ $classroom->year }}</td>
-                            <td>{{ $classroom->number }}</td>
-                            <td>{{ $classroom->section }}</td>
-                            <td><a href="{{ route("classroom_enter", $classroom->id) }}">Visit</a></td>
-                          </tr>
+                        <tr>
+                          <td>{{ $classroom->term }}</td>
+                          <td>{{ $classroom->year }}</td>
+                          <td>{{ $classroom->number }}</td>
+                          <td>{{ $classroom->section }}</td>
+                          <td><a href="{{ route("classroom_enter", $classroom->id) }}">Visit</a></td>
+                        </tr>
                       @endforeach
                     </tbody>
                   </table>
