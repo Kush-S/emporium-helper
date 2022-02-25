bcb172846216ff09d850fe04a4d1ffa1371b56c8
kush-s <26009914+Kush-S@users.noreply.github.com>
2022-02-24 23:38:13 -0500

    Add classroom members list to settings

--- resources/views/settings.blade.php
+++ resources/views/settings.blade.php
@@ -16,32 +16,54 @@
           <label>b</label>
           <input type="text" class="form-control" value="124.03443">
         </div>
-        <div class="float-end">
+        <div class="">
           <button type="submit" class="btn btn-danger">Reset</button>
           <button type="submit" class="btn btn-primary">Save</button>
         </div>
       </form>
     </div>
 
-    <span class="col-lg-6 pb-5 align-middle">
+    <span class="col-lg-6 d-flex align-items-center">
       risk = -1.06285 * grade + 124.03443
     </span>
   </div>
 
-  <div class="">
+  <div class="row">
     <div class="col-lg-4 pb-5">
       <h4><u>Email template for student notification</u></h4>
       <form>
         <div class="form-group pb-2">
           <textarea class="form-control" id="exampleFormControlTextarea1" rows="6">Dear student,
-            
+
 You are at risk...more words...</textarea>
         </div>
-        <div class="float-end">
+        <div class="">
           <button type="submit" class="btn btn-danger">Reset</button>
           <button type="submit" class="btn btn-primary">Save</button>
         </div>
       </form>
+    </div>
+    <div class="offset-md-2 col-lg-6 border">
+      <h4><u>Classroom members</u></h4>
+      <table class="table table-hover">
+        <thead>
+          <tr>
+            <th scope="col">Name</th>
+            <th scope="col">Email</th>
+          </tr>
+        </thead>
+        <tbody>
+          <tr>
+            <td>Mark Jacob</td>
+            <td>mjacob@bgsu.edu</td>
+          </tr>
+          <tr>
+            <td>Otto Thornton</td>
+            <td>othorn@bgsu.edu</td>
+          </tr>
+        </tbody>
+      </table>
+      <button type="submit" class="btn btn-primary">Add new member</button>
     </div>
   </div>
 </div>
