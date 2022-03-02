b70eb38dd3fbfff95fccb40fc38af533477f524c
kush-s <26009914+Kush-S@users.noreply.github.com>
2022-03-02 15:52:04 -0500

    Indicate required fields on classroom creation

--- resources/views/classroom_create.blade.php
+++ resources/views/classroom_create.blade.php
@@ -12,7 +12,7 @@
               @csrf
               <div class="form-group col-md-4">
                 <div class="form-group pb-3">
-                  <label for="inputState">Class Term</label>
+                  <label for="inputState">Class Term<span class="text-danger">*</span></label>
                   <select name="class_term" class="form-select" required>
                     <option selected>Spring</option>
                     <option>Summer</option>
@@ -20,16 +20,19 @@
                   </select>
                 </div>
                 <div class="form-group pb-3">
-                  <label for="file_name">Class Year</label>
+                  <label for="file_name">Class Year<span class="text-danger">*</span></label>
                   <input type="number" class="form-control" name="class_year" value="{{ now()->year }}" required></input>
                 </div>
                 <div class="form-group pb-3">
-                  <label for="file_name">Course Number</label>
+                  <label for="file_name">Course Number<span class="text-danger">*</span></label>
                   <input type="text" class="form-control" name="class_number" placeholder="Example: CS 2010" required></input>
                 </div>
                 <div class="form-group pb-3">
                   <label for="file_name">Class Section</label>
                   <input type="text" class="form-control" name="class_section" placeholder="Example: 1001"></input>
+                </div>
+                <div>
+                  <span class="text-danger">*</span> indicates required field
                 </div>
                 <div class="float-end">
                   <a href="{{route("dashboard")}}" class="btn btn-danger">Cancel</a>
