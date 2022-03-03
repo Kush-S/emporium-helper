7bf8114a4a01f975ccc7c110d2f739359bd5a0a1
kush-s <26009914+Kush-S@users.noreply.github.com>
2022-03-03 17:45:38 -0500

    Add footer design wit links

--- resources/views/components/footer.blade.php
+++ resources/views/components/footer.blade.php
@@ -1,3 +1,22 @@
-<div>
-    <!-- An unexamined life is not worth living. - Socrates -->
+<div class="bg-dark py-4">
+  <div class="row">
+    <div class="col py-2">
+      <div class="row border-end">
+        <a href="#" class="text-white d-flex justify-content-end text-decoration-none">
+          About
+        </a>
+        <a href="#" class="text-white d-flex justify-content-end text-decoration-none">
+          Credits
+        </a>
+        <a href="#" class="text-white d-flex justify-content-end text-decoration-none">
+          Tutorial
+        </a>
+      </div>
+    </div>
+    <div class="col">
+      <div class="text-white">
+        icon
+      </div>
+    </div>
+  </div>
 </div>
