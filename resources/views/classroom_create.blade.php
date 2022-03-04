f0562e3b1045fb6583330004ba9b1978a537fe17
kush-s <26009914+Kush-S@users.noreply.github.com>
2022-03-03 21:21:23 -0500

    Add image to classroom create page

--- resources/views/classroom_create.blade.php
+++ resources/views/classroom_create.blade.php
@@ -7,39 +7,44 @@
       <div class="card">
         <div class="card-header">Create a new Classroom</div>
         <div class="card-body justify-content-center">
-          <div class="">
-            <form method="POST" enctype="multipart/form-data" action="{{ route('classroom_save') }}">
-              @csrf
-              <div class="form-group col-md-4">
-                <div class="form-group pb-3">
-                  <label for="inputState">Class Term<span class="text-danger">*</span></label>
-                  <select name="class_term" class="form-select" required>
-                    <option selected>Spring</option>
-                    <option>Summer</option>
-                    <option>Fall</option>
-                  </select>
+          <div class="row">
+            <div class="col-md-6">
+              <form method="POST" enctype="multipart/form-data" action="{{ route('classroom_save') }}">
+                @csrf
+                <div class="form-group">
+                  <div class="form-group pb-3">
+                    <label for="inputState">Class Term<span class="text-danger">*</span></label>
+                    <select name="class_term" class="form-select" required>
+                      <option selected>Spring</option>
+                      <option>Summer</option>
+                      <option>Fall</option>
+                    </select>
+                  </div>
+                  <div class="form-group pb-3">
+                    <label for="file_name">Class Year<span class="text-danger">*</span></label>
+                    <input type="number" class="form-control" name="class_year" value="{{ now()->year }}" required></input>
+                  </div>
+                  <div class="form-group pb-3">
+                    <label for="file_name">Course Number<span class="text-danger">*</span></label>
+                    <input type="text" class="form-control" name="class_number" placeholder="Example: CS 2010" required></input>
+                  </div>
+                  <div class="form-group pb-3">
+                    <label for="file_name">Class Section</label>
+                    <input type="text" class="form-control" name="class_section" placeholder="Example: 1001"></input>
+                  </div>
+                  <div>
+                    <span class="text-danger">*</span> indicates required field
+                  </div>
+                  <div class="float-end">
+                    <a href="{{route("dashboard")}}" class="btn btn-danger">Cancel</a>
+                    <button type="submit" class="btn btn-primary">Submit</button>
+                  </div>
                 </div>
-                <div class="form-group pb-3">
-                  <label for="file_name">Class Year<span class="text-danger">*</span></label>
-                  <input type="number" class="form-control" name="class_year" value="{{ now()->year }}" required></input>
-                </div>
-                <div class="form-group pb-3">
-                  <label for="file_name">Course Number<span class="text-danger">*</span></label>
-                  <input type="text" class="form-control" name="class_number" placeholder="Example: CS 2010" required></input>
-                </div>
-                <div class="form-group pb-3">
-                  <label for="file_name">Class Section</label>
-                  <input type="text" class="form-control" name="class_section" placeholder="Example: 1001"></input>
-                </div>
-                <div>
-                  <span class="text-danger">*</span> indicates required field
-                </div>
-                <div class="float-end">
-                  <a href="{{route("dashboard")}}" class="btn btn-danger">Cancel</a>
-                  <button type="submit" class="btn btn-primary">Submit</button>
-                </div>
-              </div>
-            </form>
+              </form>
+            </div>
+            <div class="col-md-6 d-flex justify-content-center text-center m-auto">
+              <img src="{{ asset('images/lecture_hall.jpg') }}" style="width:80%;">
+            </div>
           </div>
         </div>
       </div>
