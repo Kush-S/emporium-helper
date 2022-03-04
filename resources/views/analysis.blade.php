759a47e16c612476ec86b75ea965946bf6e62bf0
kush-s <26009914+Kush-S@users.noreply.github.com>
2022-03-04 09:57:51 -0500

    Add classroom information to analysis page

--- resources/views/analysis.blade.php
+++ resources/views/analysis.blade.php
@@ -11,11 +11,25 @@
   </div>
   {{-- <div class="row" style="height: 300px"> --}}
   <div class="row">
-    <div class="col-md-6 text-center">
+    <div class="col-6 d-flex justify-content-center">
+        <div class="col-4 py-5">
+          <a href="{{ route('analysis_students', Request()->id) }}" class="btn btn-primary d-flex justify-content-center p-3">Student list</a>
+        </div>
+      </div>
+    <div class="col-6 d-flex justify-content-center py-2">
+      <div class="row pt-2">
+        <div class="text-center">
+          Files used: {{ count($randNums) }}
+        </div>
+        <div class="text-center">
+          Total students: {{ rand(10,30) }}
+        </div>
+        <div class="text-center">
+          <?php $randRisk =  rand(0,1) ?>
+          Students at risk: <span class="{{ $randRisk > 0 ? 'text-danger' : '' }}">{{$randRisk}}</span>
+        </div>
+      </div>
 
-    </div>
-    <div class="">
-      <a href="{{ route('analysis_students', Request()->id) }}" class="btn btn-primary">Student list</a>
     </div>
   </div>
 </div>
@@ -30,7 +44,7 @@ const ctx = document.getElementById('chart1').getContext('2d');
 const myChart1 = new Chart(ctx, {
     type: 'bar',
     data: {
-        labels: ['Red', 'Blue', 'Yellow', 'Green', 'Purple', 'Orange'],
+        labels: ['Chapter 1.csv', 'Chapter 2.csv', 'Chapter 3.csv', 'Chapter 4.csv', 'Chapter 5.csv', 'Chapter 6.csv'],
         datasets: [{
             label: '# of Votes',
             data: nums,
@@ -57,7 +71,12 @@ const myChart1 = new Chart(ctx, {
         scales: {
             y: {
                 beginAtZero: true
-            }
+            },
+            xAxes: [{
+              ticks : {
+                minRotation : 90
+              }
+            }]
         }
     }
 });
