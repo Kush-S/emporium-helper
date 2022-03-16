11a56671885531b6d9f47c75cc9de5ed738456ae
Kush-S <Kush-S>
2022-03-16 18:53:08 -0400

    Add trim() to emails array of whitelisted middlewares

--- app/Http/Middleware/UserIsWhitelisted.php
+++ app/Http/Middleware/UserIsWhitelisted.php
@@ -16,7 +16,12 @@ class UserIsWhitelisted
      */
     public function handle(Request $request, Closure $next)
     {
-      $emails = explode("\r\n", file_get_contents(base_path('whitelist.txt'), true));
+      $emails = explode("\n", file_get_contents(base_path('whitelist.txt'), true));
+
+      for($i = 0; $i < count($emails); $i++)
+      {
+        $emails[$i] = trim($emails[$i]);
+      }
 
       if(!in_array(auth()->user()->email, $emails)) {
         return redirect()->route('dashboard')->with('status', 'Email \'' . auth()->user()->email . '\' is not whitelisted. Please contact BGSU IT.');
--- app/Http/Middleware/WhitelistedForRegistering.php
+++ app/Http/Middleware/WhitelistedForRegistering.php
@@ -16,13 +16,17 @@ class WhitelistedForRegistering
      */
     public function handle(Request $request, Closure $next)
     {
-      $emails = explode("\r\n", file_get_contents(base_path('whitelist.txt'), true));
+      $emails = explode("\n", file_get_contents(base_path('whitelist.txt'), true));
+
+      for($i = 0; $i < count($emails); $i++)
+      {
+        $emails[$i] = trim($emails[$i]);
+      }
 
       if(!in_array($request->email, $emails)) {
-        return redirect()->route('register')->with('status', 'Email \'' . $request->email . '\' is not whitelisted. Please contact BGSU IT.');
+        return redirect()->route('register')->with('status', 'Email \'' . $request->email . '\' is not whitelisted. Please contact BGSU IT. List is: ' . $emails[0] . ' and ');
       }
-      else{
-        return $next($request);
-      }
+
+      return $next($request);
     }
 }
