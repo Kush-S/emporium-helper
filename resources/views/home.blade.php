a0ca416adb05146486b44de4861fb682794bac42
kush-s <26009914+Kush-S@users.noreply.github.com>
2022-03-17 19:14:04 -0400

    Add bgsu logo to home page and remove custom css min-height

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -1,7 +1,7 @@
 @extends('layouts.app')
 
 @section('content')
-<div class="container pb-4 custom-min-height">
+<div class="container pb-4">
     <div class="row">
       <div class="col-md-10 offset-md-1">
         @if (session('status'))
@@ -26,6 +26,9 @@
               <br>
               <br>
               The purpose of this app is to help instructors see which of their students are at a risk of failing the class by analyzing instructor uploaded zyBooks and Canvas grade files.
+            </div>
+            <div class="col-md-6 my-5 d-flex justify-content-center text-center m-auto">
+              <img src="{{ asset('images/bgsu_logo.png') }}" style="width:70%;">
             </div>
           </div>
         </div>
