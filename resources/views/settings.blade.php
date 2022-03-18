65bfd06eb05e0c9c927bedf994ec47cf09ed488a
kush-s <26009914+Kush-S@users.noreply.github.com>
2022-03-17 22:49:53 -0400

    Redesign settings page to use cards

--- resources/views/settings.blade.php
+++ resources/views/settings.blade.php
@@ -1,6 +1,6 @@
 <x-headers/>
 
-<div class="container d-flex justify-content-center ">
+<div class="container d-flex justify-content-center">
   <div class="row">
     <div class="pt-1 h4 pb-2 text-white bg-info rounded-pill">
       {{ $classroom->number }}
@@ -13,76 +13,102 @@
   </div>
 </div>
 
-<div class="container bg-light border rounded mb-5">
+<div class="container bg-white border rounded mb-5">
   <div class="row py-5">
-    <div class="col-lg-4">
-      <h4><u>Edit risk calculation variables</u></h4>
-      <form>
-        <div class="form-group pb-2">
-          <label>m</label>
-          <input type="text" class="form-control" value="-1.06285">
-        </div>
-        <div class="form-group pb-2">
-          <label>b</label>
-          <input type="text" class="form-control" value="124.03443">
-        </div>
-        <div class="">
-          <button type="submit" class="btn btn-danger">Cancel</button>
-          <button type="submit" class="btn btn-primary">Save</button>
-          <a href="#">reset to default</a>
-        </div>
-      </form>
+    <div class="col-md-5">
+      <strong>Edit risk calculation variables</strong><br>
+      These variables are used to calculate the risk for this classroom. The calculation is<br>
+      risk = -m * grade + b
     </div>
-
-    <span class="col-lg-6 d-flex align-items-center">
-      risk = -1.06285 * grade + 124.03443
-    </span>
-  </div>
-
-  <div class="row">
-    <div class="col-lg-4 pb-5">
-      <h4><u>Email template for student notification</u></h4>
-      <form>
-        <div class="form-group pb-2">
-          <textarea class="form-control" id="exampleFormControlTextarea1" rows="6">Dear student,
-
-You are at risk...more words...</textarea>
+    <div class="col-md-7">
+      <div class="card">
+        <h3 class="card-header">Variables</h3>
+        <div class="card-body">
+          <form>
+            <div class="form-group pb-2">
+              <label>m</label>
+              <input type="text" class="form-control" value="-1.06285">
+            </div>
+            <div class="form-group pb-2">
+              <label>b</label>
+              <input type="text" class="form-control" value="124.03443">
+            </div>
+            <div class="float-end">
+              <a href="#">reset to default</a>
+              <button type="submit" class="btn btn-danger">Cancel</button>
+              <button type="submit" class="btn btn-primary px-3">Save</button>
+            </div>
+          </form>
         </div>
-        <div class="">
-          <button type="submit" class="btn btn-danger">Cancel</button>
-          <button type="submit" class="btn btn-primary">Save</button>
-          <a href="#">reset to default</a>
-        </div>
-      </form>
-    </div>
-    <div class="offset-md-2 col-lg-6 border">
-      <h4><u>Instructors</u></h4>
-      <table class="table table-hover">
-        <thead>
-          <tr>
-            <th scope="col">Name</th>
-            <th scope="col">Email</th>
-            <th scope="col"></th>
-          </tr>
-        </thead>
-        <tbody>
-          <tr>
-            <td>{{ $owner->name }} (owner)</td>
-            <td>{{ $owner->email }}</td>
-          </tr>
-          @foreach ($instructors as $instructor)
-            @if ($instructor->email != $owner->email)
-            <tr>
-              <td>{{ $instructor->name }}</td>
-              <td>{{ $instructor->email }}</td>
-              <td><a href="{{ route('settings_remove_instructor', [Request()->id, $instructor->id]) }}" class="btn btn-danger">Remove</a></td>
-            </tr>
-            @endif
-          @endforeach
-        </tbody>
-      </table>
-      <a href="{{ route('settings_add_instructor', Request()->id) }}" class="btn btn-primary">Add new instructor</a>
+      </div>
+
     </div>
   </div>
+
+  <div class="row py-5">
+    <div class="col-md-5">
+      <strong>Email template for student notifications</strong><br>
+      This email template is used when sending a student a notification from this app. This template is specific to this classroom.
+    </div>
+      <div class="col-md-7">
+        <div class="card">
+          <h3 class="card-header">Email Template</h3>
+          <div class="card-body">
+            <form>
+              <div class="form-group pb-2">
+                <textarea class="form-control" id="exampleFormControlTextarea1" rows="6">Dear student,
+
+    You are at risk...more words...</textarea>
+              </div>
+              <div class="float-end">
+                <a href="#">reset to default</a>
+                <button type="submit" class="btn btn-danger">Cancel</button>
+                <button type="submit" class="btn btn-primary">Save</button>
+              </div>
+            </form>
+          </div>
+        </div>
+      </div>
+  </div>
+
+  <div class="row pb-4">
+    <div class="col-md-5">
+      <strong>Email template for student notifications</strong><br>
+      This email template is used when sending a student a notification from this app. This template is specific to this classroom.
+    </div>
+    <div class="col-md-7">
+      <div class="card">
+        <h3 class="card-header">Email Template</h3>
+        <div class="card-body">
+          <table class="table table-hover">
+            <thead>
+              <tr>
+                <th scope="col">Name</th>
+                <th scope="col">Email</th>
+                <th scope="col"></th>
+              </tr>
+            </thead>
+            <tbody>
+              <tr>
+                <td>{{ $owner->name }} (owner)</td>
+                <td>{{ $owner->email }}</td>
+              </tr>
+              @foreach ($instructors as $instructor)
+              @if ($instructor->email != $owner->email)
+                <tr>
+                  <td>{{ $instructor->name }}</td>
+                  <td>{{ $instructor->email }}</td>
+                  <td><a href="{{ route('settings_remove_instructor', [Request()->id, $instructor->id]) }}" class="btn btn-danger">Remove</a></td>
+                </tr>
+              @endif
+              @endforeach
+            </tbody>
+          </table>
+          <a href="{{ route('settings_add_instructor', Request()->id) }}" class="btn btn-primary">Add new instructor</a>
+        </div>
+      </div>
+    </div>
+  </div>
+
 </div>
 <x-footer/>
