0ff6dd7083f127aef88369df0de0ef2ad29a0eb1
kush-s <26009914+Kush-S@users.noreply.github.com>
2022-03-24 20:59:04 -0400

    Style student list page and add columns

--- resources/views/analysis_student_list.blade.php
+++ resources/views/analysis_student_list.blade.php
@@ -1,35 +1,48 @@
 @extends('layouts.app')
 
 @section('content')
-  <div class="container">
-    <div class="pb-2">
-      <a href="{{ route('analysis_index', Request()->id) }}" class="btn btn-primary">Back to analysis</a>
-    </div>
+
+<div class="container border rounded my-5">
+  <div class="row">
+    <a href="{{ route('analysis_index', Request()->id) }}" class="btn btn-primary col-2 m-2">Back to analysis</a>
   </div>
-  <div class="container bg-light border rounded">
+  <div class="row">
+    <div class="text-center">
+      <h4>Student Risk Statistics</h4>
+    </div>
+
     <div class="row">
-      <div class="p-5 d-flex justify-content-center">
-        <h4>Student Risk Statistics</h4>
-      </div>
-      <div>
+      <div class="col-6 mx-auto">
         <table class="table table-striped table-hover">
           <thead>
             <tr>
               <th scope="col">Name</th>
               <th scope="col">Current risk</th>
+              <th scope="col">Participation</th>
+              <th scope="col">Lab</th>
+              <th scope="col">Challenge</th>
             </tr>
           </thead>
 
           <tbody>
             @for($i = 1; $i <= 10; $i++)
               <tr>
-                <th scope="row"><a href="{{ route('analysis_student_info', Request()->id) }}">John Doe {{$i}}</a></th>
-                <td>{{rand(0,10)}}%</td>
+                <div class="">
+                  <td scope="row"><a href="{{ route('analysis_student_info', Request()->id) }}">John Doe {{$i}}</a></td>
+                  <td>{{10 - $i}}%</td>
+                  <td>{{rand(80,100)}}%</td>
+                  <td>{{rand(80,100)}}%</td>
+                  <td>{{rand(80,100)}}%</td>
+                </div>
               </tr>
             @endfor
           </tbody>
         </table>
       </div>
     </div>
+
   </div>
+</div>
+
+<x-footer/>
 @endsection
