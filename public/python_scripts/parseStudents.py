ae4974abc51a3d1c7c65a1a5f8aba7edb44bcc32
kush-s <26009914+Kush-S@users.noreply.github.com>
2022-03-27 20:17:09 -0400

    Add python script to get zyBooks file data

--- public/python_scripts/parseStudents.py
+++
@@ -1,7 +0,0 @@
-import pandas as pd
-import sys
-
-data_frame = pd.read_csv(str(sys.argv[1]))
-
-json = data_frame[['Last name', 'First name', 'Primary email']].to_json(orient='index')
-print(json)
---
+++ public/python_scripts/parseZybooks.py
@@ -0,0 +1,22 @@
+import pandas as pd
+import sys
+
+df = pd.read_csv(str(sys.argv[1]))
+
+df2 = pd.DataFrame()
+df2["First name"] = df["First name"]
+df2["Last name"] = df["Last name"]
+df2["Primary email"] = df["Primary email"]
+df2["School email"] = df["School email"]
+df2["Student ID"] = df["Student ID"]
+
+# Get only participation/challenge/lab total columns, not the time ones
+df2["Participation total"] = df.loc[:, df.columns.str.match("(Participation total)(?!.time)(.+)")]
+df2["Challenge total"] = df.loc[:, df.columns.str.match("(Challenge total)(?!.time)(.+)")]
+df2["Lab total"] = df.loc[:, df.columns.str.match("(Lab total)(?!.time)(.+)")]
+
+# Calculate risk of each student based on participation, challenge, and lab grade
+df2['Risk'] = 100 - (df2['Participation total'] / 20 + df2['Challenge total'] / 20 + df2['Lab total'] / 1.11)
+
+json = df2.to_json(orient='index')
+print(json)
