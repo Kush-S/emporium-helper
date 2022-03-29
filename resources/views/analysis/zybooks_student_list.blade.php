bbdc2618b1a272345c3bf8d911d8d31fd92d932f
kush-s <26009914+Kush-S@users.noreply.github.com>
2022-03-29 15:50:17 -0400

    Add % sign to risk on student list analysis page

--- resources/views/analysis/zybooks_student_list.blade.php
+++ resources/views/analysis/zybooks_student_list.blade.php
@@ -1,7 +1,6 @@
 @extends('layouts.app')
 
 @section('content')
-
 <div class="container border rounded my-5">
   <div class="row">
     <a href="{{ route('analysis_index', Request()->id) }}" class="btn btn-primary col-2 m-2">Back to analysis</a>
@@ -31,7 +30,7 @@
                 <div>
                   <td>{{ $student['First name']}} {{ $student['Last name']}}</td>
                   <td>{{ $student['Primary email']}}</td>
-                  <td class="{{ $student['Risk'] > 30 ? 'text-danger h5' : '' }}">{{ $student['Risk']}}</td>
+                  <td class="{{ $student['Risk'] > 30 ? 'text-danger h5' : '' }}">{{ $student['Risk']}}%</td>
                   <td>{{ $student['Participation total']}}</td>
                   <td>{{ $student['Challenge total']}}</td>
                   <td>{{ $student['Lab total']}}</td>
@@ -45,6 +44,5 @@
 
   </div>
 </div>
-
 <x-footer/>
 @endsection
