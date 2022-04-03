ac44fcf0cac311611d00d7357ca5853e4f454395
kush-s <26009914+Kush-S@users.noreply.github.com>
2022-04-03 13:30:38 -0400

    Add Mail sending structure

---
+++ app/Mail/StudentNotification.php
@@ -0,0 +1,34 @@
+<?php
+
+namespace App\Mail;
+
+use Illuminate\Bus\Queueable;
+use Illuminate\Contracts\Queue\ShouldQueue;
+use Illuminate\Mail\Mailable;
+use Illuminate\Queue\SerializesModels;
+
+class StudentNotification extends Mailable
+{
+    use Queueable, SerializesModels;
+
+    /**
+     * Create a new message instance.
+     *
+     * @return void
+     */
+    public function __construct()
+    {
+        //
+    }
+
+    /**
+     * Build the message.
+     *
+     * @return $this
+     */
+    public function build()
+    {
+        return $this->from('zycat1969@gmail.com', 'zyCat')
+                    ->view('mailNotification');
+    }
+}
