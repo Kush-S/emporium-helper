66ecb5d1bab65741f8a89ff28356fefb01072ea0
Kush-S <Kush-S>
2022-04-09 15:25:15 -0400

    Add text to footer credit and about page

--- resources/views/footer_about.blade.php
+++ resources/views/footer_about.blade.php
@@ -8,15 +8,15 @@
           <div class="card-header">About</div>
           <div class="card-body">
             <div class="row justify-content-center pb-2">
-              This app is, and does...
+              zyCat is an application design to help Computer Science instructors at BGSU determine if a student is at risk of failing the class.
             </div>
 
             <div class="row justify-content-center mt-4">
-              For...
+              Early Computer Science courses form a foundation from which all future CS classes build from. Without a strong foundation built during the early classes, a student is likely to have problems in future CS classes, and might not be able to finish the degree.
             </div>
 
             <div class="row justify-content-center mt-4">
-              Why...
+              zyCat works by analysing zyBooks and Canvas grade files which are provided by the instructor, and shows the risk of every student in the class.
             </div>
 
             <div class="col-md-6 my-5 d-flex justify-content-center text-center m-auto">
--- resources/views/footer_credits.blade.php
+++ resources/views/footer_credits.blade.php
@@ -8,15 +8,19 @@
           <div class="card-header">Credits</div>
           <div class="card-body">
             <div class="row justify-content-center pb-2">
-              This app was made by...
+              zyCat was made by Kshitij (Kush) Saxena during Spring 2022, and presented during the poster presentation on April 15, 2022.
             </div>
 
             <div class="row justify-content-center mt-4">
-              For...
+              This advisor for this project was Dr. Chao.
             </div>
 
             <div class="row justify-content-center mt-4">
-              Why/when...
+              The photos on the classroom create page were taken by Dr. Chao.
+            </div>
+
+            <div class="row justify-content-center mt-4">
+              The app logo was created using: https://express.adobe.com/express-apps/logomaker/
             </div>
 
             <div class="col-md-6 my-5 d-flex justify-content-center text-center m-auto">
